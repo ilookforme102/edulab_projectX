ab885b525182bf10940a5c97375535e9078d70e6
sumankharel <javelin.suman@gmail.com>
2016-11-03 14:46:20 +0200

    added layout in material format

--- app/app.js
+++ app/app.js
@@ -14,7 +14,7 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
 */
-var App = angular.module('myApp',['ui.router','ngMaterial']);
+var App = angular.module('myApp',['ui.router','ngMaterial', 'ngMessages']);
 
 App.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
   // For any unmatched url, send to /business
@@ -70,5 +70,44 @@ App.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
         $scope.data.cb3 = false;
         $scope.data.cb4 = false;
         $scope.data.cb5 = false;
+        $scope.data.cb6 = false;
+        $scope.data.cb7 = false;
+        $scope.data.cb8 = false;
+        $scope.data.cb9 = false;
+        $scope.data.cb10 = false;
+        $scope.data.cb11 = false;
+        $scope.data.cb12 = false;
+        $scope.data.cb13 = false;
+        $scope.data.cb14 = false;
+        $scope.data.cb15 = false;
+        $scope.data.cb16 = false;
+        $scope.data.cb17 = false;
+        $scope.data.cb18 = false;
+        $scope.data.cb19 = false;
+        $scope.data.cb20 = false;
+        $scope.data.cb21 = false;
+
+
 
     })
+    .controller('DateCtrl', function($scope) {
+        $scope.myDate = new Date();
+
+        $scope.minDate = new Date(
+            $scope.myDate.getFullYear(),
+            $scope.myDate.getMonth() - 2,
+            $scope.myDate.getDate());
+
+        $scope.maxDate = new Date(
+            $scope.myDate.getFullYear(),
+            $scope.myDate.getMonth() + 2,
+            $scope.myDate.getDate());
+
+        $scope.onlyWeekendsPredicate = function(date) {
+            var day = date.getDay();
+            return day === 0 || day === 6;
+        };
+    })
+    .controller('titleCtrl', function() {
+
+    })  ;
