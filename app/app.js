f248b588c17ee8493403d535676332a9ec898cc2
kushal atreya <kushalatreya@kushals-MacBook-Pro.local>
2016-11-08 12:32:41 +0200

    done for the day

--- app/app.js
+++ app/app.js
@@ -1,25 +1,66 @@
 'use strict';
-// Declare app level module which depends on views, and components
-var myApp = angular.module('myApp',['ui.router','ngMaterial','ngMessages'])
-.config(['$stateProvider', '$urlRouterProvider','$mdThemingProvider', function($stateProvider, $urlRouterProvider,$mdThemingProvider){
-    // For any unmatched url, send to /business
-    $mdThemingProvider
-        .theme('default')
-        .primaryPalette('blue')
-        .accentPalette('pink');
-    $urlRouterProvider.otherwise("/login");
-    $stateProvider
-        .state('login', {//State demonstrating Nested views
-            url: "/login",
-            templateUrl: "login/login.html"
-        })
-        .state('home', {//State demonstrating Nested views
-            url: "/home",
-            templateUrl: "view1/view1.html"
-        })
-        .state('progress', {//State demonstrating Multiple,named views
-            url: "/progress",
-            templateUrl: "view2/view2.html"
 
-        })
+// Declare app level module which depends on views, and components
+/*
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+*/
+var App = angular.module('myApp',['ui.router','ngMaterial'
+]);
+
+App.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+  // For any unmatched url, send to /business
+  $urlRouterProvider.otherwise("/business");
+
+  $stateProvider
+      .state('business', {//State demonstrating Nested views
+        url: "/business",
+        templateUrl: "view1/view1.html"
+      })
+      /*.state('business.products', {//nested state [products is the nested state of business state]
+        url: "/products",
+        templateUrl: "products.html",
+        controller: function($scope){
+          $scope.products = ["Computer", "Printers", "Phones", "Bags"];
+        }
+      })
+      .state('business.services', {//nested state [services is the nested state of business state]
+        url: "/services",
+        templateUrl: "services.html",
+        controller: function($scope){
+          $scope.services = ["Selling", "Support", "Delivery", "Reparation"];
+        }
+      })
+*/
+      .state('portfolio', {//State demonstrating Multiple,named views
+        url: "/portfolio",
+          templateUrl: "view2/view2.html"
+       /* views: {
+          ""  :    { templateUrl: "view2.html" },
+          "view1@portfolio": { template: "Write whatever you want, it's your virtual company." },
+          "view2@portfolio": { templateUrl: "clients.html" ,
+            controller: function($scope){
+              $scope.clients = ["HP", "IBM", "MicroSoft"];
+            }
+          }
+        }          }*/
+
+      })
+}])
+    .controller('AppCtrl', function($scope) {
+        $scope.title1 = 'Button';
+        $scope.title4 = 'Warn';
+        $scope.isDisabled = true;
+
+        $scope.googleUrl = 'http://google.com';
+
+    });
