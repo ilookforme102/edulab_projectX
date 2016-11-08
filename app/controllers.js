4bd17028fc701d3db1ba349fffa346487ca05021
kushal atreya <kushalatreya@kushals-MacBook-Pro.local>
2016-11-08 12:29:58 +0200

    update.

--- app/controllers.js
+++
@@ -1,8 +0,0 @@
-myApp.controller('AppCtrl', function($scope) {
-    $scope.title1 = 'Button';
-    $scope.title4 = 'Warn';
-    $scope.isDisabled = true;
-
-    $scope.googleUrl = 'http://google.com';
-
-});
--- app/leftNavDirective.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Khoa's PC on 11/4/2016.
- */
-myApp.directive('left-nav', function() {
-    return {
-        templateUrl: 'left-nav.html'
-    };
-});
