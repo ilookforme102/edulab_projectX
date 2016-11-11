e1f398f1f9873f53ce2a10673abc2c64ffb05b55
Khoa's PC <ilookforme102@gmail.com>
2016-11-11 10:58:59 +0200

    merge with Krishna

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -34,3 +34,31 @@ myApp.controller('leftnavCtrl', function ($scope) {
 
 
 });
+//Krishna progress
+myApp.controller('mainController', function($scope) {
+    $scope.sortType     = 'name'; // set the default sort type
+    $scope.sortReverse  = false;  // set the default sort order
+    $scope.searchname   = '';     // set the default search/filter term
+
+    // create the list of student
+    $scope.progress_table = [
+        { name: 'tom', student_comment: '', student_progress: 2 },
+        { name: 'dick', student_comment: '', student_progress: 4 },
+        { name: 'harry', student_comment: '', student_progress: 7 },
+        { name: 'thomas', student_comment: '', student_progress: 6 },
+        { name: 'john', student_comment: '', student_progress: 2 },
+        { name: 'enna', student_comment: '', student_progress: 4 },
+        { name: 'catherine', student_comment: '', student_progress: 7 },
+        { name: 'pekka', student_comment: '', student_progress: 6 },
+        { name: 'kari', student_comment: '', student_progress: 2 },
+        { name: 'ninna', student_comment: '', student_progress: 4 },
+        { name: 'teemu', student_comment: '', student_progress: 7 },
+        { name: 'milton', student_comment: '', student_progress: 6 },
+        { name: 'merkel', student_comment: '', student_progress: 2 },
+        { name: 'tyoop', student_comment: '', student_progress: 4 },
+        { name: 'ming', student_comment: '', student_progress: 7 },
+        { name: 'eddie', student_comment: '', student_progress: 6 }
+
+
+
+    ]});
